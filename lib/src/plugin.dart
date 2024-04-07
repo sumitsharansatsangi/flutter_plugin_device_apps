5809ab7c48bff8a9781c468948d4beab4c5819ee
sumitsharansatsangi <sharansumitkumar@gmail.com>
2024-04-07 20:57:37 +0530

    code updated

--- lib/src/plugin.dart
+++ lib/src/plugin.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
@@ -25,9 +24,9 @@ class DeviceApps {
   /// [onlyAppsWithLaunchIntent] will only list applications when an entrypoint.
   /// It is similar to what a launcher will display
   static Future<List<Application>> getInstalledApplications({
-    bool includeSystemApps: false,
-    bool includeAppIcons: false,
-    bool onlyAppsWithLaunchIntent: false,
+    bool includeSystemApps = false,
+    bool includeAppIcons = false,
+    bool onlyAppsWithLaunchIntent= false,
   }) async {
     try {
       final Object apps =
